f849be52a3cd5a9ab1b5eec9be6ebc7ddd1f2a9b
adiWieli <dewa17a@gmail.com>
2018-07-27 20:13:35 +0700

    asd

--- resources/views/master/master_intinerary/create_intinerary.blade.php
+++ resources/views/master/master_intinerary/create_intinerary.blade.php
@@ -530,41 +530,7 @@
 
 
 
-    $('#chooseFile').bind('change', function () {
-        var filename = $("#chooseFile").val();
-        var fsize = $('#chooseFile')[0].files[0].size;
-        if(fsize>1048576) //do something if file size more than 1 mb (1048576)
-        {
-          return false;
-        }
-        if (/^\s*$/.test(filename)) {
-            $(".file-upload").removeClass('active');
-            $("#noFile").text("No file chosen..."); 
-        }
-        else {
-            $(".file-upload").addClass('active');
-            $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
-        }
-    });
 
-    var loadFile = function(event) {
-        var fsize = $('#chooseFile')[0].files[0].size;
-        if(fsize>2048576) //do something if file size more than 1 mb (1048576)
-        {
-          iziToast.warning({
-            icon: 'fa fa-times',
-            position:'topRight',
-            message: 'File Is To Big!',
-          });
-          return false;
-        }
-        var reader = new FileReader();
-        reader.onload = function(){
-            var output = document.getElementById('output');
-            output.src = reader.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    };
 
 
     $(document).on('click','.save',function(){
