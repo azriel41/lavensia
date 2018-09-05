151973c507b1a1b573fd0c4655d14400367a55d7
adiyani17a <wielijarni@gmail.com>
2018-09-05 21:23:10 +0700

    asd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,24 +26,24 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
 
-            $data = DB::table('d_booking')
-                      ->whereRaw('db_total_remain = db_total')
-                      ->where('db_status','Waiting List')
-                      ->get();
+            // $data = DB::table('d_booking')
+            //           ->whereRaw('db_total_remain = db_total')
+            //           ->where('db_status','Waiting List')
+            //           ->get();
 
-            for ($i=0; $i < count($data); $i++) { 
-                $cari = DB::table('m_detail_intinerary')
-                          ->where('md_id',$data[$i]->db_intinerary_id)
-                          ->first();
+            // for ($i=0; $i < count($data); $i++) { 
+            //     $cari = DB::table('m_detail_intinerary')
+            //               ->where('md_id',$data[$i]->db_intinerary_id)
+            //               ->first();
 
-                $update = DB::table('m_detail_intinerary')
-                          ->where('md_id',$data[$i]->db_intinerary_id)
-                          ->update([
-                            'md_seat_remain' => $cari->md_seat_remain + $data[$i]->db_total_adult + $data[$i]->db_total_child
-                          ]);
-            }
+            //     $update = DB::table('m_detail_intinerary')
+            //               ->where('md_id',$data[$i]->db_intinerary_id)
+            //               ->update([
+            //                 'md_seat_remain' => $cari->md_seat_remain + $data[$i]->db_total_adult + $data[$i]->db_total_child
+            //               ]);
+            // }
 
-
+            return 'tes';
         })->everyMinute();
     }
 
