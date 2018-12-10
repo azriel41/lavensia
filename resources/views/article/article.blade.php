8ed024125b14ef514320a57e13f6dcb6b7ea1293
AzrieL/Muttq <denyprasetyo41@gmail.com>
2018-12-10 16:22:48 +0700

    Update article.blade.php

--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -164,51 +164,13 @@
           <!-- aside -->
           <div class="col-md-4">
             <!-- ad -->
-            <div class="aside-widget text-center">
+            <div class="text-center">
               <a href="#" style="display: inline-block;margin: auto;">
                 <img class="img-responsive" src="./img/ad-1.jpg" alt="">
               </a>
             </div>
-            <!-- /ad -->
-
-            <!-- post widget -->
-         {{--    <div class="aside-widget">
-              <div class="section-title">
-                <h2>Most Read</h2>
-              </div>
-
-              <div class="post post-widget">
-                <a class="post-img" href="blog-post.html"><img src="./img/widget-1.jpg" alt=""></a>
-                <div class="post-body">
-                  <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-                </div>
-              </div>
-
-              <div class="post post-widget">
-                <a class="post-img" href="blog-post.html"><img src="./img/widget-2.jpg" alt=""></a>
-                <div class="post-body">
-                  <h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
-                </div>
-              </div>
-
-              <div class="post post-widget">
-                <a class="post-img" href="blog-post.html"><img src="./img/widget-3.jpg" alt=""></a>
-                <div class="post-body">
-                  <h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
-                </div>
-              </div>
-
-              <div class="post post-widget">
-                <a class="post-img" href="blog-post.html"><img src="./img/widget-4.jpg" alt=""></a>
-                <div class="post-body">
-                  <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-                </div>
-              </div>
-            </div> --}}
-            <!-- /post widget -->
-
-            <!-- post widget -->
-            <div class="aside-widget">
+         
+            <div class="">
               <div class="section-title">
                 <h2>Like us on Facebook</h2>
               </div>
