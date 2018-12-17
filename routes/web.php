088d739bbb69e6760d52b20cef38b3489d07acd8
AzrieL/Muttq <denyprasetyo41@gmail.com>
2018-12-17 11:11:38 +0700

    123
    1

--- routes/web.php
+++ routes/web.php
@@ -306,7 +306,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/master/master_intinerary/ganti_nama', 'master\intinerary_controller@ganti_nama')->name('ganti_nama');
 	Route::get('/master/master_intinerary/delete', 'master\intinerary_controller@delete')->name('delete_intinerary');
 	Route::get('/master/master_intinerary/approve', 'master\intinerary_controller@approve')->name('approve_itinerary');
-	Route::get('/master/master_intinerary/intinerary_detail', 'master\intinerary_controller@datatale_report_customer_agent')->name('intinerary_detail');
+	Route::get('/master/master_intinerary/intinerary_detail', 'master\intinerary_controller@intinerary_detail')->name('intinerary_detail');
 	Route::post('/master/master_intinerary/save_detail', 'master\intinerary_controller@save_detail')->name('save_detail');
 
 
