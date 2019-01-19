a527180c95f414e751087faeb658f971c64a0e5a
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-01-19 17:47:33 +0700

    1
    1

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Mail;
+use App\Notifications\VerifyEmail;
 class RegisterController extends Controller
 {
     /*
@@ -78,4 +79,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    $user->notify(new VerifyEmail($user));
+
 }
