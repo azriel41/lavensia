7bf4ba32577e0244a1df90d60503c5452d879d7d
AzrieL/Muttq <denyprasetyo41@gmail.com>
2019-01-20 17:02:23 +0700

    1

--- app/Http/Controllers/master/intinerary_controller.php
+++ app/Http/Controllers/master/intinerary_controller.php
@@ -333,8 +333,8 @@ class intinerary_controller extends Controller
 
     public function departure(Request $req)
     {
-        $data = $this->all_variable->intinerary()->same('mi_id',$req->mc_id)->orderBy('md_start','ASC');
-
+        $data = $this->all_variable->intinerary()->same('mi_id',$req->mc_id);
+        return $data;
         return view('master.master_intinerary.table_departure',compact('data'));
 
     }
